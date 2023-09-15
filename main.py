b14d8f3bc12cd8e1d781debfce60197ddd24318c
KlugSolutions <tomasz.kluge@gmail.com>
2023-09-15 23:18:03 +0200

    Completed project

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 from tkinter import *
 import random
 
@@ -6,21 +5,8 @@ t = Tk()
 t.title("Choose button")
 t.geometry("300x350")
 
-
-def insert_buttons():
-    quantity = 8
-    global buttons
-    buttons = []
-    correct = random.randint(0, quantity-1)
-    for i in range(quantity):
-        if i == correct:
-            buttons.append(Button(t, text="click me", command=win))
-        else:
-            buttons.append(Button(t, text="click me", command=loose))
-    for i in buttons:
-        i.pack(fill=BOTH, expand=YES)
-
-    insert_buttons()
+buttons = []
+label = None
 
 
 def win():
@@ -32,17 +18,35 @@ def win():
     t.after(5000, restart)
 
 
+def loose():
+    for i in buttons:
+        i.destroy()
+    global label
+    label = Label(t, text="Wrong button")
+    label.pack(fill=BOTH, expand=YES)
+    t.after(1000, restart)
+
+
+def insert_buttons():
+    quantity = 5
+    global buttons
+    correct = random.randint(0, quantity-1)
+    for i in range(quantity):
+        if i == correct:
+            buttons.append(Button(t, text="click me", command=win))
+        else:
+            buttons.append(Button(t, text="click me", command=loose))
+    for i in buttons:
+        i.pack(fill=BOTH, expand=YES)
+
+
+insert_buttons()
+
+
 def restart():
     label.destroy()
+    buttons.clear()
     insert_buttons()
 
 
-def crate():
-    for i in buttons:
-        i.destroy()
-        global label
-        label = Label(t, text="Wrong button")
-        label.pack(fill=BOTH, expand=YES)
-        t.after(5000, restart)
-
-
+t.mainloop()
