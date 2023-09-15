2dca3660b7166e43449b3f97f5b2f8f307da8985
KlugSolutions <tomasz.kluge@gmail.com>
2023-09-15 22:50:11 +0200

    added def win, restart, crate

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ t = Tk()
 t.title("Choose button")
 t.geometry("300x350")
 
+
 def insert_buttons():
     quantity = 8
     global buttons
@@ -13,9 +14,35 @@ def insert_buttons():
     correct = random.randint(0, quantity-1)
     for i in range(quantity):
         if i == correct:
-            buttons.append(Button(t, text="click me", command="win"))
+            buttons.append(Button(t, text="click me", command=win))
         else:
-            buttons.append(Button(t, text="click me", command="loose"))
+            buttons.append(Button(t, text="click me", command=loose))
+    for i in buttons:
+        i.pack(fill=BOTH, expand=YES)
 
+    insert_buttons()
+
+
+def win():
+    for i in buttons:
+        i.destroy()
+    global label
+    label = Label(t, text="Correct button")
+    label.pack(fill=BOTH, expand=YES)
+    t.after(5000, restart)
+
+
+def restart():
+    label.destroy()
+    insert_buttons()
+
+
+def crate():
+    for i in buttons:
+        i.destroy()
+        global label
+        label = Label(t, text="Wrong button")
+        label.pack(fill=BOTH, expand=YES)
+        t.after(5000, restart)
 
 
