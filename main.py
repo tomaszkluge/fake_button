0c46822b0a4b95bfb3612da6c6c0bd637e46518f
KlugSolutions <tomasz.kluge@gmail.com>
2023-09-15 22:27:17 +0200

    new def - insert_buttons

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import random
 from tkinter import *
+import random
+
+t = Tk()
+t.title("Choose button")
+t.geometry("300x350")
+
+def insert_buttons():
+    quantity = 8
+    global buttons
+    buttons = []
+    correct = random.randint(0, quantity-1)
+    for i in range(quantity):
+        if i == correct:
+            buttons.append(Button(t, text="click me", command="win"))
+        else:
+            buttons.append(Button(t, text="click me", command="loose"))
+
 
 
